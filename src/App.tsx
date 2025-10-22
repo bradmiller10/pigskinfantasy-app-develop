a9bbef66010a9f904afb4c2d3ff1790321014705
bradmiller10 <bmiller10@gmail.com>
2025-10-22 10:31:12 -0500

    Update App.tsx
    add simple test screen to confirm web build

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,19 @@
-import React, { FC, useState } from "react";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ThemeProvider } from "@/style";
-import { User, UserRole } from "@/typings";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import AppLoading from "expo-app-loading";
-import { Auth, Draft, Home, Error, Admin, Manage } from "./pages";
+import React from "react";
+import { Text, View } from "react-native";
 
-const Stack = createStackNavigator();
-
-export const App: FC = () => {
-  // @todo replace with auth checking
-  const [user, setUser] = useState<User>({
-    name: {
-      first: "John",
-      last: "Doe",
-    },
-    phoneNumber: "+18176004439",
-    role: UserRole.User,
-    leagues: [],
-  });
-
-  if (typeof user === "undefined") return <AppLoading />;
-
-  console.log("Got user", user);
+export default function App() {
   return (
-    <ThemeProvider>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName={user.role ? "Home" : "Auth"}>
-            <Stack.Screen name="Auth" component={Auth} />
-            <Stack.Screen name="Draft" component={Draft} />
-            <Stack.Screen name="Error" component={Error} />
-            <Stack.Screen name="Home" component={Home} />
-            {user.role === UserRole.Owner && (
-              <Stack.Screen name="Admin" component={Admin} />
-            )}
-            {user.role !== UserRole.User && user.leagues.length && (
-              <Stack.Screen name="Manage" component={Manage} />
-            )}
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </ThemeProvider>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f2f2f2",
+      }}
+    >
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+        Pigskin Fantasy U — Web Test Working!
+      </Text>
+    </View>
   );
-};
+}
